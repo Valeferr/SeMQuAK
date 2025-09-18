c190e003b319a93e351cceb4c8a45276ed0ae464
Valentina Ferrentino <v.ferrentino10@studenti.unisa.it>
2025-09-18 17:49:14 +0200

    minor

--- config/profile_attributes.py
+++ config/profile_attributes.py
@@ -7,225 +7,225 @@ from rdflib import VOID
 """
 
 profile_attributes = {
-    'KG id': {
-        'predicate': DCTERMS.identifier,
-        'datatype': XSD.string,
-        'access_methods': [META]
+    "KG id": {
+        "predicate": DCTERMS.identifier,
+        "datatype": XSD.string,
+        "access_methods": [META]
     },
 
-    'KG name': {
-        'predicate': DCTERMS.title,
-        'datatype': XSD.string,
-        'access_methods': [SPA]
+    "KG name": {
+        "predicate": DCTERMS.title,
+        "datatype": XSD.string,
+        "access_methods": [SPA]
     },
 
-    'Description': {
-        'predicate': DCTERMS.description,
-        'datatype': XSD.string,
-        'access_methods': [SPA]
+    "Description": {
+        "predicate": DCTERMS.description,
+        "datatype": XSD.string,
+        "access_methods": [SPA]
     },
 
-    'Languages (metadata)': {
-        'predicate': DCTERMS.language,
-        'datatype': XSD.string,
-        'access_methods': [META]
+    "Languages (metadata)": {
+        "predicate": DCTERMS.language,
+        "datatype": XSD.string,
+        "access_methods": [META]
     },
 
-    'Offline dumps': {
-        'predicate': EX.offlineDumps,
-        'datatype': XSD.string,
-        'access_methods': [META]
+    "Offline dumps": {
+        "predicate": EX.offlineDumps,
+        "datatype": XSD.string,
+        "access_methods": [META]
     },
 
-    'Vocabularies': {
-        'predicate': VOID.vocabulary,
-        'datatype': XSD.string,
-        'access_methods': [SPA, VO]
+    "Vocabularies": {
+        "predicate": VOID.vocabulary,
+        "datatype": XSD.string,
+        "access_methods": [SPA, VO]
     },
 
-    'Contributor': {
-        'predicate': DCTERMS.contributor,
-        'datatype': XSD.string,
-        'access_methods': [META]
+    "Contributor": {
+        "predicate": DCTERMS.contributor,
+        "datatype": XSD.string,
+        "access_methods": [META]
     },
 
-    'URL for download the dataset': {
-        'predicate': VOID.dataDump,
-        'datatype': XSD.string,
-        'access_methods': [META, SPA, VO]
+    "URL for download the dataset": {
+        "predicate": VOID.dataDump,
+        "datatype": XSD.string,
+        "access_methods": [META, SPA, VO]
     },
 
-    'External links': {
-        'predicate': EX.hasExternalLinks,
-        'datatype': XSD.string,
-        'access_methods': [SPA]
+    "External links": {
+        "predicate": EX.hasExternalLinks,
+        "datatype": XSD.string,
+        "access_methods": [SPA]
     },
 
-    'Dataset update frequency': {
-        'predicate': EX.accrualPeriodicity,
-        'datatype': XSD.string,
-        'access_methods': [META]
+    "Dataset update frequency": {
+        "predicate": EX.accrualPeriodicity,
+        "datatype": XSD.string,
+        "access_methods": [META]
     },
 
-    'Sources': {
-        'predicate': DCTERMS.source,
-        'datatype': XSD.string,
-        'access_methods': [META]
+    "Sources": {
+        "predicate": DCTERMS.source,
+        "datatype": XSD.string,
+        "access_methods": [META]
     },
 
-    'Publisher': {
-        'predicate': DCTERMS.publisher,
-        'datatype': XSD.string,
-        'access_methods': [SPA]
+    "Publisher": {
+        "predicate": DCTERMS.publisher,
+        "datatype": XSD.string,
+        "access_methods": [SPA]
     },
 
-    'Dataset URL': {
-        'predicate': DCAT.accessURL,
-        'datatype': XSD.anyURI,
-        'access_methods': [SPA]
+    "Dataset URL": {
+        "predicate": DCAT.accessURL,
+        "datatype": XSD.anyURI,
+        "access_methods": [SPA]
     },
 
-    'Sparql endpoint': {
-        'predicate': VOID.sparqlEndpoint,
-        'datatype': XSD.string,
-        'access_methods': [SPA]
+    "Sparql endpoint": {
+        "predicate": VOID.sparqlEndpoint,
+        "datatype": XSD.string,
+        "access_methods": [SPA]
     },
 
-    'SPARQL endpoint URL': {
-        'predicate': DCAT.endpointURL,
-        'datatype': XSD.anyURI,
-        'access_methods': [SPA]
+    "SPARQL endpoint URL": {
+        "predicate": DCAT.endpointURL,
+        "datatype": XSD.anyURI,
+        "access_methods": [SPA]
     },
 
-    'Author (query)': {
-        'predicate': DCTERMS.creator,
-        'datatype': XSD.string,
-        'access_methods': [SPA]
+    "Author (query)": {
+        "predicate": DCTERMS.creator,
+        "datatype": XSD.string,
+        "access_methods": [SPA]
     },
 
-    'Author (metadata)': {
-        'predicate': DCTERMS.creator,
-        'datatype': XSD.string,
-        'access_methods': [META]
+    "Author (metadata)": {
+        "predicate": DCTERMS.creator,
+        "datatype": XSD.string,
+        "access_methods": [META]
     },
 
-    'License machine redeable (metadata)': {
-        'predicate': DCTERMS.license,
-        'datatype': XSD.anyURI,
-        'access_methods': [META]
+    "License machine redeable (metadata)": {
+        "predicate": DCTERMS.license,
+        "datatype": XSD.anyURI,
+        "access_methods": [META]
     },
 
-    'License machine redeable (query)': {
-        'predicate': DCTERMS.license,
-        'datatype': XSD.anyURI,
-        'access_methods': [SPA]
+    "License machine redeable (query)": {
+        "predicate": DCTERMS.license,
+        "datatype": XSD.anyURI,
+        "access_methods": [SPA]
     },
 
-    'License human redeable': {
-        'predicate': EX.hasHumanReadableLicense,
-        'datatype': XSD.boolean,
-        'access_methods': [SPA]
+    "License human redeable": {
+        "predicate": EX.hasHumanReadableLicense,
+        "datatype": XSD.boolean,
+        "access_methods": [SPA]
     },
 
-    'Use HTTPS': {
-        'predicate': EX.usesHTTPS,
-        'datatype': XSD.boolean,
-        'access_methods': [SPA]
+    "Use HTTPS": {
+        "predicate": EX.usesHTTPS,
+        "datatype": XSD.boolean,
+        "access_methods": [SPA]
     },
 
-    'Requires authentication': {
-        'predicate': EX.requiresAuthentication,
-        'datatype': XSD.boolean,
-        'access_methods': [META]
+    "Requires authentication": {
+        "predicate": EX.requiresAuthentication,
+        "datatype": XSD.boolean,
+        "access_methods": [META]
     },
 
-    'Availability of a common accepted Media Type': {
-        'predicate': EX.availebilityMediaType,
-        'datatype': XSD.boolean,
-        'access_methods': [META]
+    "Availability of a common accepted Media Type": {
+        "predicate": EX.availebilityMediaType,
+        "datatype": XSD.boolean,
+        "access_methods": [META]
     },
 
-    'Availability of RDF dump (query)': {
-        'predicate': VOID.dataDump,
-        'datatype': XSD.decimal,
-        'access_methods': [SPA, VO]
+    "Availability of RDF dump (query)": {
+        "predicate": VOID.dataDump,
+        "datatype": XSD.decimal,
+        "access_methods": [SPA, VO]
     },
 
-    'Availability of RDF dump (metadata)': {
-        'predicate': VOID.dataDump,
-        'datatype': XSD.boolean,
-        'access_methods': [META, VO]
+    "Availability of RDF dump (metadata)": {
+        "predicate": VOID.dataDump,
+        "datatype": XSD.boolean,
+        "access_methods": [META, VO]
     },
 
-    'Inactive links': {
-        'predicate': EX.inactiveLinks,
-        'datatype': XSD.boolean,
-        'access_methods': [META]
+    "Inactive links": {
+        "predicate": EX.inactiveLinks,
+        "datatype": XSD.boolean,
+        "access_methods": [META]
     },
 
-    'Availability for download (query)' : {
-        'predicate': EX.availabilityForDownload,
-        'datatype': XSD.boolean,
-        'access_methods': [SPA]
+    "Availability for download (query)" : {
+        "predicate": EX.availabilityForDownload,
+        "datatype": XSD.boolean,
+        "access_methods": [SPA]
     },
 
-    'Availability VoID file': {
-        'predicate': EX.availabilityVoIDFile,
-        'datatype': XSD.string,
-        'access_methods': [META]
+    "Availability VoID file": {
+        "predicate": EX.availabilityVoIDFile,
+        "datatype": XSD.string,
+        "access_methods": [META]
     },
 
-    'Uses RDF structures': {
-        'predicate': EX.usesRDFStructures,
-        'datatype': XSD.boolean,
-        'access_methods': [META]
+    "Uses RDF structures": {
+        "predicate": EX.usesRDFStructures,
+        "datatype": XSD.boolean,
+        "access_methods": [META]
     },
 
-    'Ontology Hijacking problem': {
-        'predicate': EX.hasOntologyHijacking,
-        'datatype': XSD.boolean,
-        'access_methods': [SPA]
+    "Ontology Hijacking problem": {
+        "predicate": EX.hasOntologyHijacking,
+        "datatype": XSD.boolean,
+        "access_methods": [SPA]
     },
 
-    'Limited': {
-        'predicate': EX.isLimited,
-        'datatype': XSD.boolean,
-        'access_methods': [SPA]
+    "Limited": {
+        "predicate": EX.isLimited,
+        "datatype": XSD.boolean,
+        "access_methods": [SPA]
     },
 
-    'Is on a trusted provider list': {
-        'predicate': EX.isTrustedProvider,
-        'datatype': XSD.boolean,
-        'access_methods': [META]
+    "Is on a trusted provider list": {
+        "predicate": EX.isTrustedProvider,
+        "datatype": XSD.boolean,
+        "access_methods": [META]
     },
 
-    'Age of data': {
-        'predicate': DCTERMS.created,
-        'datatype': XSD.dateTime,
-        'access_methods': [SPA, VO]
+    "Age of data": {
+        "predicate": DCTERMS.created,
+        "datatype": XSD.dateTime,
+        "access_methods": [SPA, VO]
     },
 
-    'Presence of example': {
-        'predicate': EX.hasExample,
-        'datatype': XSD.boolean,
-        'access_methods': [META]
+    "Presence of example": {
+        "predicate": EX.hasExample,
+        "datatype": XSD.boolean,
+        "access_methods": [META]
     },
 
-    'metadata-media-type': {
-        'predicate': DCAT.mediaType,
-        'datatype': XSD.string,
-        'access_methods': [META]
+    "metadata-media-type": {
+        "predicate": DCAT.mediaType,
+        "datatype": XSD.string,
+        "access_methods": [META]
     },
 
-    'Languages (query)': {
-        'predicate': DCTERMS.language,
-        'datatype': XSD.string,
-        'access_methods': [SPA]
+    "Languages (query)": {
+        "predicate": DCTERMS.language,
+        "datatype": XSD.string,
+        "access_methods": [SPA]
     },
 
-    'Serialization formats': {
-        'predicate': DCTERMS.format,
-        'datatype': XSD.string,
-        'access_methods': [META]
+    "Serialization formats": {
+        "predicate": DCTERMS.format,
+        "datatype": XSD.string,
+        "access_methods": [META]
     },
 }
